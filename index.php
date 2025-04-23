b37cce865d0f5821133e34f4e9ce3c883c5ec228
fmoreno <fmoreno@senasa.es>
2025-04-23 13:52:06 +0200

    25-04-23 13:51 Proyecto actualizado

--- index.php
+++ index.php
@@ -6,10 +6,12 @@
     <title>Git2</title>
 </head>
 <body>
-    Curso GIT<br>
+    GIT<br>
     <a href="clientes.php">pulsa aqui para ia a clientes</a>
     <br>
-    <a href="ventas.php">pulsa aqui para ir a ventas</a>
+    <a href="ventas.php">Pulsa aqui para ir a ventas</a>
+    <br>
+    <a href="compras.php">pulsa aqui para ir a compras</a>
 
 </body>
 </html>
